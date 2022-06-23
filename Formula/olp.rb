926d92d06a1d650929b950083e1488ea3c65e288
Dmitry Abramov <47140652+dmitriy-abramov@users.noreply.github.com>
2022-06-23 12:13:27 +0000

    Add version 12.1.2

--- Formula/olp.rb
+++ Formula/olp.rb
@@ -1,9 +1,9 @@
 class Olp < Formula
   desc "With the Command Line Interface you can access HERE platform services from a command line or with scripts."
   homepage "https://platform.in.here.com/sdk/cli"
-  url "https://artifact.api.platform.here.com/v1/references/hrn:here:artifact:::com.here.platform:cli:11.1.0/cli-11.1.0.gz\?apiKey\=84q8N9DRx6YAqyJaXUpr9fhiTnCUIBzvHLpk3QU4Oaw"
-  sha256 "cc2443e8c8ae6599e5ca4b35cc4d2f42b4eb902471d44195e7a3b6b19bcf8f57"
-  version "11.1.0"
+  url "https://artifact.api.platform.here.com/v1/references/hrn:here:artifact:::com.here.platform:cli:12.1.2/cli-12.1.2.gz\?apiKey\=84q8N9DRx6YAqyJaXUpr9fhiTnCUIBzvHLpk3QU4Oaw"
+  sha256 "4acacfaca744b00b2ddfca3c7fd9506a7aae9b01fe21dfe6ffc0fd7f1c18711f"
+  version "12.1.2"
   license "Proprietary"
 
   depends_on "openjdk@8"
@@ -11,8 +11,8 @@ class Olp < Formula
   def install
     prefix.install "HERE_NOTICE"
     prefix.install "LICENSE"
-    libexec.install "cli_2.12-11.1.0.jar"
-    bin.write_jar_script libexec/"cli_2.12-11.1.0.jar", "olp", "-Dfile.encoding=UTF8"
+    libexec.install "cli_2.12-12.1.2.jar"
+    bin.write_jar_script libexec/"cli_2.12-12.1.2.jar", "olp", "-Dfile.encoding=UTF8"
   end
 
   test do
